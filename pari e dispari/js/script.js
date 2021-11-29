f4abafc39d14e1b0cf90ece6330ef45cc571f08c
salzanopm <salzanopm@email.com>
2021-11-29 23:13:54 +0100

    correzione pari e dispari

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -1,43 +1,84 @@
-// Pari e Dispari
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-// Sommiamo i due numeri
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-// Dichiariamo chi ha vinto.
+// // PARI E DISPARI
 
-// Chiedo il numero all'utente
-let userNumber = parseInt( prompt('Dimmi un numero') );
-// console.log(userNumber);
+// // CONSEGNA
 
-// Valuto se il numero è pari o dispari
-// la variabile userNumberEvenOrOdd è uguale al return della funzione evenOrOdd
-let userNumberEvenOrOdd = evenOrOdd(userNumber);
-console.log(userNumberEvenOrOdd);
+// // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// // Sommiamo i due numeri
+// // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// // Dichiariamo chi ha vinto.
 
-// Se è pari, scrive 'Il numero è pari', altrimenti scrivo 'Il numero è dispari'
-if( userNumberEvenOrOdd === 'pari' ) {
-    alert('Il numero è pari');
-} else {
-    alert('Il numero è dispari');
+// // ANALISI
+
+// // [] L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+//     // [x] chiedo all'utente di decidere tra pari o dispari
+//     // [x] utente immette pari o dispari
+// const userChoice = prompt('scegli tra pari o dispari');
+// // console.log(userChoice);
+// if (userChoice === 'pari') {
+//     console.log('hai scelto', userChoice)
+// } else {
+//     console.log('hai scelto', userChoice)
+// }
+// // [] chiedo all'utente di selezionare un numero tra 1 e 5 
+//     // - se numero compreso tra 1 e 5 procedo
+//     // - altrimenti continuo a richiedere numero
+//     let userNumber= 0;
+
+
+// chiedo all'utente di scegliere tra pari o dispari
+let userEvenOrOdd = prompt('Scegli pari o dispari');
+
+// cnontinuo a chiedere all'utente di reinserire un numero corretto
+while (( userEvenOrOdd !== 'pari') && ( userEvenOrOdd !== 'dispari')) {
+    userEvenOrOdd = prompt('Scegli pari o dispari');
+    console.log(userEvenOrOdd)
 }
+
+// chiedo all'utente di scegliere un numero tra 1 e 5
+let userNumber = parseInt( prompt('Scegli un numero tra 1 e 5'));
+
+// cnontinuo a chiedere all'utente di reinserire un numero corretto
+while (userNumber > 5 || userNumber < 1) {
+    userNumber = parseInt( prompt('Scegli un numero tra 1 e 5'));
+    // console.log(userNumber)
+}
+
+// genero un numero casuale per il CPU (funzione)
+const cpuNumber = getRndInteger(1, 5);
+
+// Sommo i due numeri
+const sum = userNumber + cpuNumber;
+
+// stabilire se la somma è pari o dispari
+const sumEvenOrOdd = isNumberEvenOrOdd(sum);
+
+// stabilire il vincitore
+if(userEvenOrOdd === sumEvenOrOdd) {
+    alert("ha vinto l'user");
+} else {
+    alert ("ha vinto il CPU");
+}
+
 
 
 // FUNCTIONS
 
-// Stabilisce se un numero è pari o dispari
-// number -> numero da valutare (se pari o dispari)
-// 
-// return: una stringa. Se pari la stringa è uguale a 'pari' altrimenti 'dispari'
-function evenOrOdd(number) {
-    let result;
+// funzione numero random da 1 a 5
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
 
-    // fare if se % 2 === 0
-    if( number % 2 === 0 ) {
+// funzione riconosce numeri pari o dispari
+// number -> numero da valutare
+
+// return: stringa pari se numero pari o dispari se numero dispari
+function isNumberEvenOrOdd(number) {
+    let result;
+    if (number % 2 === 0) {
         result = 'pari';
     } else {
         result = 'dispari';
     }
-
-    // return
     return result;
 }
